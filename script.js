aece1ec283d3c4abfcd86d1ad7ff01d63c6db832
dbs <dbs@foxmail.com>
2026-07-13 14:59:25 +0800

    在点击后加入了一段暑假祝福

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
     function changeText() {
-    document.getElementById('msg').textContent = '你刚刚触发了一段 JavaScript。';
+    document.getElementById('msg').textContent = '你刚刚触发了一段 JavaScript,and,暑假快乐！杜乐天。';
     }
